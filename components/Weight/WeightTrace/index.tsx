bbc26ddd428665996ed001264d5b9e543f6cd06f
Wong Yu Fei <ywong045@e.ntu.edu.sg>
2025-02-07 11:28:09 +0800

    ignore typescript

--- components/Weight/WeightTrace/index.tsx
+++ components/Weight/WeightTrace/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useEffect, useRef } from "react";
 import NeoVis, { NeovisConfig } from "neovis.js";
 import { useRouter } from "next/navigation";
